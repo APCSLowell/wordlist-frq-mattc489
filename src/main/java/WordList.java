ef116b1fce0c701d95adf72424447f47dae95e2f
mattc489 <machan12@s.sfusd.edu>
2025-04-10 11:11:07 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,21 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+   int count=0;
+    for(int i=0; i<myList.size(); i++){
+      if(myList.get(i).length()==len){
+        count++;}
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
+    Arraylist<String> r = new Arraylist<String>();
+    for(int i=0; i<myList.length(); i++){
+      String word = myList;
+      if(word.length()!=len){
+        list.remove(word);
 
   }
 }
